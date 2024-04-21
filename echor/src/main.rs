dbe3f96deab71ea7d8f6f1a9a0d2477fb020c87e
Russ Gunther <russell.g@leantaas.com>
2024-04-20 20:35:52 -0400

    Chap 2: Rewrite echo by deriving from clap::Parser

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,29 +1,23 @@
-use clap::{Arg, ArgAction, Command};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `echo`
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    /// Do not print newline
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
 
 fn main() {
-    let _matches = Command::new("echor")
-        .version("0.1.0")
-        .author("Russ Gunther <not-posting-my-email-publically@gmail.com>")
-        .about("Rust version of echo")
-        .arg(
-            Arg::new("text")
-                .value_name("TEXT")
-                .help("Input text")
-                .required(true)
-                .num_args(1..),
-        )
-        .arg(
-            Arg::new("omit_newline")
-                .short('n')
-                .action(ArgAction::SetTrue)
-                .help("Do not print newline"),
-        )
-        .get_matches();
-
-    let text: Vec<String> = _matches.get_many("text").unwrap().cloned().collect();
-    let omit_newline = _matches.get_flag("omit_newline");
-
-    let ending = if omit_newline { "" } else { "\n" };
-
-    print!("{}{ending}", text.join(" "))
+    let args = Args::parse();
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { "" } else { "\n" }
+    );
 }
